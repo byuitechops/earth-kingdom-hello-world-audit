a1d6d452d1c0cdec746e88a5c7a066b7175486bb
Seth Bolander <bol15015@byui.edu>
2019-07-08 15:11:40 -0600

    added info to coursecode loop

--- MyAudit.cs
+++ MyAudit.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Byui.CourseAudits.Business.Model.AuditBaseClass;
 using Byui.CourseAudits.Business.Model.AuditReporters;
+using Newtonsoft.JSON;
+using Newtonsoft.JSON.Linq;
 
 namespace Byui.CourseAudits.Business.Model.Audits
 {
@@ -25,8 +27,26 @@ namespace Byui.CourseAudits.Business.Model.Audits
             var auditResults = new List<AuditResult>();
             foreach (var courseCode in courseCodes)
             {
-                // our code goes here
+                var start = DateTime.Now;
+                // make api call here (/api/v1/courses/:course_id/settings)
+                var result = true; // = api_call.GetValue("hide_final_grades");
+                var currentAudit = new AuditResult
+                {
+                    AuditCode = Code,
+                    courseCode = courseCode,
+                    StartTime = start,
+                    EndTime = DateTime.Now,
+                    AuditStatus = result
+                };
+                currentAudit.Messages.Add(new AuditMessage
+                {
+                    AuditCode = Code,
+                    MessageType = "example",
+                    Message = $"Made an api call to course {courseCode} settings for \"Is hide_final_grades true?\""
+                });
+                auditResults.Add(currentAudit);
             }
+            return auditResults;
         }
     }
 }
