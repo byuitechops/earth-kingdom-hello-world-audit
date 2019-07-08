e79785103136a8e85e000e3e21ad15ef3cc8fcad
Seth Bolander <bol15015@byui.edu>
2019-07-08 14:19:03 -0600

    added MyAudit.cs

---
+++ MyAudit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Byui.CourseAudits.Business.Model.AuditBaseClass;
+using Byui.CourseAudits.Business.Model.AuditReporters;
+
+namespace Byui.CourseAudits.Business.Model.Audits
+{
+    public class HideFinalGradesAudit : Audit
+    {
+        public string Name;
+        public string Code;
+        public string Description;
+        private readonly IAuditReporter _auditReporter;
+        public HideFinalGradesAudit(IAuditReporter auditReporter) : base(auditReporter)
+        {
+            Name = "Hide Final Grades Audit";
+            Code = "Hide-final-grades-audit";
+            Description = "This is a test audit";
+        }
+        public void ReportAuditResults(List<AuditResult> auditResults);
+        public List<AuditResult> RunAudits(List<string> courseCodes);
+        internal override List<AuditResult> ExecuteAudits(List<string> courseCodes)
+        {
+
+        }
+    }
+}
