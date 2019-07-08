a0951c28f03d1b6e44a2a23375845cc8f92f6004
KayleeHartzog <har15031@byui.edu>
2019-07-08 15:30:36 -0600

    made the api call

--- MyAudit.cs
+++ MyAudit.cs
@@ -29,6 +29,36 @@ namespace Byui.CourseAudits.Business.Model.Audits
             {
                 var start = DateTime.Now;
                 // make api call here (/api/v1/courses/:course_id/settings)
+                string api_token = System.Environment.GetEnvironmentVariable("CANVAS_API_TOKEN");
+                string queryString = args.Length != 0 ? args[0] : "/api/v1/courses/" + courseCode + "/settings";
+
+                private static readonly HttpClient client = new HttpClient();
+                public static async Task<string> MakeHTTPRequest(string queryString, string api_token)
+                {
+                    using (client)
+                    {
+                        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", api_token);
+                        // Call asynchronous network methods in a try/catch block to handle exceptions
+                        try
+                        {
+                            if (queryString[0] != '/')
+                            {
+                                queryString = "/" + queryString;
+                            }
+                            string url = "https://byui.instructure.com" + queryString;
+                            string responseBody = await client.GetStringAsync(url);
+
+                            return responseBody;
+                        }
+                        catch (HttpRequestException e)
+                        {
+                            Console.WriteLine("There was an error: " + e.Message);
+                            throw;
+                        }
+                    }
+                }
+                string callData = await HTTPHelper.MakeHTTPRequest(queryString, api_token);
+                dynamic data = JsonConvert.DeserializeObject(result);
                 var result = true; // = api_call.GetValue("hide_final_grades");
                 var currentAudit = new AuditResult
                 {
